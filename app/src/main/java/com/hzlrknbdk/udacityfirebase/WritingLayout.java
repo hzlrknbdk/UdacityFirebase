36818ba20ac4ee8d5b37bb60c1f26f8da3ff8f61
hzlrknbdk <hzlrknbdk@gmail.com>
2019-05-08 22:53:41 +0300

    Upload and Download
    Signed-off-by: hzlrknbdk <hzlrknbdk@gmail.com>

--- app/src/main/java/com/hzlrknbdk/udacityfirebase/WritingLayout.java
+++ app/src/main/java/com/hzlrknbdk/udacityfirebase/WritingLayout.java
@@ -1,26 +1,53 @@
 package com.hzlrknbdk.udacityfirebase;
 
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+
+import java.io.File;
+import java.io.IOException;
 
 public class WritingLayout extends AppCompatActivity {
 
-    Button write;
-    Button read;
-    TextView textView;
-    FirebaseDatabase database;
-    DatabaseReference myRef;
+    private Button write;
+    private Button read;
+    private Button imagebtn;
+    private Button imageCek;
+    private ImageView imageView;
+    private TextView textView;
+    private FirebaseDatabase database;
+    private DatabaseReference myRef;
+
+
+    private FirebaseStorage storage = FirebaseStorage.getInstance();
+    private StorageReference reference = storage.getReference();
+    final File localfile = File.createTempFile("images", "jpg");
+
+    public WritingLayout() throws IOException {
+    }
 
 
     public void writeNewUser(String userId, String name, String email) {
@@ -34,12 +61,50 @@ public class WritingLayout extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_writing_layout);
 
+
         write = findViewById(R.id.write);
         read = findViewById(R.id.read);
+        imagebtn = findViewById(R.id.image);
+        imageCek = findViewById(R.id.imageCek);
+        imageView = findViewById(R.id.imageView);
         textView = findViewById(R.id.textView);
+
         database = FirebaseDatabase.getInstance();
         final DatabaseReference oku = FirebaseDatabase.getInstance().getReference().child("user").child("45236");
         myRef = database.getReference();
+
+
+        imageCek.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final StorageReference indir = reference.child("418445");
+                indir.getFile(localfile).addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        Bitmap bitmap = BitmapFactory.decodeFile(localfile.getAbsolutePath());
+                        imageView.setImageBitmap(bitmap);
+
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception exception) {
+
+                    }
+                });
+            }
+        });
+
+
+        imagebtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setType("image/*");
+                startActivityForResult(intent, 1);
+            }
+        });
+
 
         write.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -69,5 +134,28 @@ public class WritingLayout extends AppCompatActivity {
         });
     }
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1 && resultCode == RESULT_OK) {
 
+            Uri uri = data.getData();
+            StorageReference riversRef = reference.child(uri.getLastPathSegment());
+
+
+            riversRef.putFile(uri)
+                    .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                        @Override
+                        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                            Toast.makeText(WritingLayout.this, "Yükleme başarılı...", Toast.LENGTH_SHORT).show();
+                        }
+                    })
+                    .addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception exception) {
+
+                        }
+                    });
+        }
+    }
 }
